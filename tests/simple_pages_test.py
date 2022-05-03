142c1ae33f0430234da5fef810b3dd09610509de
ad828 <ad828@njit.edu>
2022-05-03 14:55:20 -0400

    pytests

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,15 +1,5 @@
 """This test the homepage"""
 
-def test_request_main_menu_links(client):
-    """This makes the index page"""
-    response = client.get("/")
-    assert response.status_code == 200
-    assert b'href="/about"' in response.data
-    assert b'href="/welcome"' in response.data
-    assert b'href="/login"' in response.data
-    assert b'href="/register"' in response.data
-
-
 def test_request_index(client):
     """This makes the index page"""
     response = client.get("/")
@@ -21,12 +11,6 @@ def test_request_about(client):
     response = client.get("/about")
     assert response.status_code == 200
     assert b"About" in response.data
-
-def test_request_page1(client):
-    """This makes the index page"""
-    response = client.get("/welcome")
-    assert response.status_code == 200
-    assert b"welcome" in response.data
 
 def test_request_page_not_found(client):
     """This makes the index page"""
